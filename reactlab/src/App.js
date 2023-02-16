378c9fe2c5498e0c470e66367b0084c49276b97a
r-scott25 <rachelbernal2@gmail.com>
2023-02-16 10:22:01 -0600

    passing props practice

---
+++ reactlab/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="George" />
+             <Heading firstName="Galaxy"/>
+        </div>
+    );
+};
+
+export default App;
